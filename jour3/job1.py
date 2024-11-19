7a440865398cceb135fbb0933f83dcc75bbee36f
unknown <ahmed-abdoulanzize.soilahoudine@laplateforme.io>
2024-11-19 14:41:57 +0100

    afficher  la grille

---
+++ jour3/job1.py
@@ -0,0 +1,6 @@
+def afficher_grille(grille):
+    print(f"{grille[0]} | {grille[1]} | {grille[2]}")
+    print("--|---|--")
+    print(f"{grille[3]} | {grille[4]} | {grille[5]}")
+    print("--|---|--")
+    print(f"{grille[6]} | {grille[7]} | {grille[8]}")
